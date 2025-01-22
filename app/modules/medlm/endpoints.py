2b7913b931b2a4581dd85a5b301a3753ea206413
Pedro Marques <pedrovitor.marques@gmail.com>
2025-01-22 14:34:36 -0300

    feat: Refactor MedLM endpoint to use aiohttp for async requests

--- app/modules/medlm/endpoints.py
+++ app/modules/medlm/endpoints.py
@@ -1,4 +1,4 @@
-import requests
+import aiohttp
 import json
 from fastapi import APIRouter
 from typing import Annotated
@@ -9,7 +9,6 @@ from app.utils.googlecloud import get_access_token
 from app.config import SMS_PROJECT_ID
 from app.modules.medlm.models import MedLMInput, MedLMOutput
 
-
 router = APIRouter()
 
 @router.post("/medlm/prompt")
@@ -17,26 +16,27 @@ async def prompt_medlm(
     input: MedLMInput,
     jwt_payload: Annotated[str, Depends(decode_token)],
 ) -> MedLMOutput:
-    
-    try:
-        result = requests.post(
-            url=f"https://us-central1-aiplatform.googleapis.com/v1/projects/{SMS_PROJECT_ID}/locations/us-central1/publishers/google/models/{input.model}:predict",
-            headers={
-                "Authorization": f"Bearer {get_access_token()}",
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            data=json.dumps({
-                "instances": [dict(instance) for instance in input.instances],
-                "parameters": dict(input.parameters) if input.parameters else None
-            }),
-            timeout=500
-        )
-        result.raise_for_status()
-    except requests.exceptions.Timeout:
-        raise HTTPException(status_code=500, detail="Timeout while waiting for MedLM response")
-    except requests.exceptions.HTTPError as e:
-        raise HTTPException(status_code=500, detail=f"HTTP error: {e}")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error: {e}")
+    url = f"https://us-central1-aiplatform.googleapis.com/v1/projects/{SMS_PROJECT_ID}/locations/us-central1/publishers/google/models/{input.model}:predict"
+    headers = {
+        "Authorization": f"Bearer {get_access_token()}",
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    payload = {
+        "instances": [dict(instance) for instance in input.instances],
+        "parameters": dict(input.parameters) if input.parameters else None
+    }
 
-    return MedLMOutput(**result.json())
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, json=payload, timeout=500) as response:
+                if response.status != 200:
+                    error_detail = await response.text()
+                    raise HTTPException(status_code=500, detail=f"HTTP error: {response.status} - {error_detail}")
+                
+                result = await response.json()
+                return MedLMOutput(**result)
+
+    except aiohttp.ClientError as e:
+        raise HTTPException(status_code=500, detail=f"Client error: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {e}")
